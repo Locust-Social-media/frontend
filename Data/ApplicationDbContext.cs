4639f9cb5bec16baa5feca4cedab1e549ce0fefe
Jelle van den Broek <je.van.den.broek@outlook.com>
2026-01-08 15:29:48 +0100

    Login page toegevoegd

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Locust.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Locust.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
