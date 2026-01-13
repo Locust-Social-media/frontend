7e37c9deaa0bb4ee1d84552bcc2c3c61283c7b9e
andorgast <andorgast@gmail.com>
2026-01-13 14:26:11 +0100

    Comments removing and test data adding

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -10,7 +10,7 @@ namespace Locust.Pages
         public PostViewModel[] Posts { get; private set; }
         public void OnGet()
         {
-            Posts = [new PostViewModel("test", "bleh", 1, 1), new PostViewModel("test2", "bleh2", 1, 1) ];
+            Posts = [new PostViewModel("Test Title No 1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 1, 1), new PostViewModel("Test Title No 2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 999999999, 999999999), new PostViewModel("Test Title No 3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 500, 30) ];
         }
     }
 }
