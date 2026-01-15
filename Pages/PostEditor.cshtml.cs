b6377175d79fdafc45ed33f8875f3355f8033ec0
andorgast <andorgast@gmail.com>
2026-01-15 10:42:34 +0100

    Page making for uploading posts

---
+++ Pages/PostEditor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Locust.Pages
+{
+    public class PostEditorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
